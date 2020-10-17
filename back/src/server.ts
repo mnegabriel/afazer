81cc62213db099618c1de78782d7eedede2a3ed2
Gabriel Dantas <gabrielmd123@gmail.com>
2020-10-17 09:29:54 -0300

    vercel hosting

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log(' 👏 server started on port 3333');
+app.listen(port, () => {
+  console.log(`👏 server started on port ${port}`);
 });
